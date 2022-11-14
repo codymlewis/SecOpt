7cf85ff151550e972174b3793a60d2089758d289
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2022-11-14 12:09:06 +1100

    Fix experiment script

--- mitigation/experiment.sh
+++ mitigation/experiment.sh
@@ -5,9 +5,9 @@ for oneshot_flag in "" "--one-shot"; do
     for model in "lenet" "cnn1" "cnn2"; do
         for dataset in "mnist" "cifar10" "svhn"; do
             for seed in {0..10}; do
-                python main.py -d $1 -s $seed $oneshot_flag -m $model -d $dataset -n 100
+                python main.py -s $seed $oneshot_flag -m $model -d $dataset -n 100
                 for eps in 0.01 0.05 0.1 0.3 0.5 1.0; do
-                    python main.py -d $1 --hardening pgd --eps $eps -s $seed $oneshot_flag -m $model -d $dataset -n 100
+                    python main.py --hardening pgd --eps $eps -s $seed $oneshot_flag -m $model -d $dataset -n 100
                 done
             done
         done
