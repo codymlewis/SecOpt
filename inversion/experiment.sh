53a686be8d0e7f2c88c983c799908a60bea5f89a
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2022-11-16 15:23:08 +1100

    Formatting

--- inversion/experiment.sh
+++ inversion/experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for dataset in "mnist" "cifar10" "svhn"; do
-	for model in "lenet" "cnn1" "cnn2"; do
-            for opt in "sgd" "adam"; do
-		for seed in {0..10}; do
-			python main.py -m $model -d $dataset -s $seed -n 100 -o $opt
-		done
+    for model in "lenet" "cnn1" "cnn2"; do
+        for opt in "sgd" "adam"; do
+            for seed in {0..10}; do
+                python main.py -m $model -d $dataset -s $seed -n 100 -o $opt
             done
-	done
+        done
+    done
 done
