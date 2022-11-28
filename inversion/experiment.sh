63b921db53221bc031241311e83bbb149d0d490e
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2022-11-28 11:32:00 +1100

    Memory considerations

--- inversion/experiment.sh
+++ inversion/experiment.sh
@@ -4,7 +4,11 @@ for dataset in "mnist" "cifar10" "svhn"; do
     for model in "lenet" "cnn1" "cnn2"; do
         for opt in "sgd" "adam"; do
             for seed in {0..3}; do
-                python main.py -m $model -d $dataset -s $seed -n 100 -o $opt -r 750
+                if [ $model = "cnn2" ]; then
+                    python main.py -m $model -d $dataset -s $seed -n 75 -o $opt -r 750
+                else
+                    python main.py -m $model -d $dataset -s $seed -n 100 -o $opt -r 750
+                fi
             done
         done
     done
