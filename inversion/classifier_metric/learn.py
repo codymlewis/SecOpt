e9a02c1c080b3c834de94d3a1a0a1e2a6aaa88f5
Cody Lewis <hello@codymlewis.com>
2023-02-14 15:56:39 +1100

    Moved dataset loading in CM to seperate file

--- inversion/classifier_metric/learn.py
+++ inversion/classifier_metric/learn.py
@@ -1,7 +1,6 @@
 from typing import Any, Tuple, NamedTuple
 from argparse import ArgumentParser
 
-import datasets
 from flax import serialization
 from flax.core.frozen_dict import freeze, FrozenDict
 import jax
@@ -9,9 +8,10 @@ import jax.numpy as jnp
 from jax import Array
 import numpy as np
 import optax
-import einops
 from tqdm import trange
 
+
+import datalib
 import cnn
 import densenet
 import resnetv2
@@ -55,83 +55,6 @@ def accuracy(model, variables, X, Y, batch_size=1000):
     return acc / jnp.ceil(ds_size / batch_size)
 
 
-def load_dataset(name: str) -> datasets.Dataset:
-    match name:
-        case "mnist": return load_mnist()
-        case "cifar10": return load_cifar10()
-        case "svhn": return load_svhn()
-        case _: raise NotImplementedError(f"Dataset {name} is not implemented")
-
-
-def load_mnist() -> datasets.Dataset:
-    """
-    Load the Fashion MNIST dataset http://arxiv.org/abs/1708.07747
-
-    Arguments:
-    - seed: seed value for the rng used in the dataset
-    """
-    ds = datasets.load_dataset("fashion_mnist")
-    ds = ds.map(
-        lambda e: {
-            'X': einops.rearrange(np.array(e['image'], dtype=np.float32) / 255, "h (w c) -> h w c", c=1),
-            'Y': e['label']
-        },
-        remove_columns=['image', 'label']
-    )
-    features = ds['train'].features
-    features['X'] = datasets.Array3D(shape=(28, 28, 1), dtype='float32')
-    ds['train'] = ds['train'].cast(features)
-    ds['test'] = ds['test'].cast(features)
-    ds.set_format('numpy')
-    return ds
-
-
-def load_cifar10() -> datasets.Dataset:
-    """
-    Load the CIFAR-10 dataset https://www.cs.toronto.edu/~kriz/cifar.html
-
-    Arguments:
-    - seed: seed value for the rng used in the dataset
-    """
-    ds = datasets.load_dataset("cifar10")
-    ds = ds.map(
-        lambda e: {
-            'X': np.array(e['img'], dtype=np.float32) / 255,
-            'Y': e['label']
-        },
-        remove_columns=['img', 'label']
-    )
-    features = ds['train'].features
-    features['X'] = datasets.Array3D(shape=(32, 32, 3), dtype='float32')
-    ds['train'] = ds['train'].cast(features)
-    ds['test'] = ds['test'].cast(features)
-    ds.set_format('numpy')
-    return ds
-
-
-def load_svhn() -> datasets.Dataset:
-    """
-    Load the SVHN dataset http://ufldl.stanford.edu/housenumbers/
-
-    Arguments:
-    - seed: seed value for the rng used in the dataset
-    """
-    ds = datasets.load_dataset("svhn", "cropped_digits")
-    ds = ds.map(
-        lambda e: {
-            'X': np.array(e['image'], dtype=np.float32) / 255,
-            'Y': e['label']
-        },
-        remove_columns=['image', 'label']
-    )
-    features = ds['train'].features
-    features['X'] = datasets.Array3D(shape=(32, 32, 3), dtype='float32')
-    ds['train'] = ds['train'].cast(features)
-    ds['test'] = ds['test'].cast(features)
-    ds.set_format('numpy')
-    return ds
-
-
 def train_step(opt, loss):
     @jax.jit
     def apply(state, opt_state, X, Y):
@@ -149,7 +72,7 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--model', type=str, default="DenseNet121", help="Model to train.")
     args = parser.parse_args()
 
-    ds = load_dataset(args.dataset)
+    ds = datalib.load_dataset(args.dataset)
     X, Y = ds['train']['X'], ds['train']['Y']
 
     if args.model == "CNN":
