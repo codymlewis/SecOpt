bb0eb304067604714d9a1abc573f7cc6c0ba8a4d
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2023-02-28 21:10:14 +1100

    Fixed script error

--- performance/experiment.sh
+++ performance/experiment.sh
@@ -7,7 +7,7 @@ for aggregation in "fedavg" "fedadam" "adam" "secagg" "ours"; do
             for clients in "10" "100"; do
                 for alpha in "0.1" "0.5" "1.0" "10.0"; do
                     for epochs in "1" "5" "10" "30"; do
-                        rounds = $(( 3000 / $epochs ))
+                        rounds=$(( 3000 / $epochs ))
                         for seed in {0..2}; do
                                 python main.py -n $clients -m $model -d $dataset -s $seed -a $aggregation -r $rounds -e $epochs --iid $alpha --efficient
                         done
