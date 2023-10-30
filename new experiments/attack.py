4e0cddfe718cc952dd68bfaf6c56b626bbd8db8a
Cody Lewis <hello@codymlewis.com>
2023-10-30 13:55:17 +1100

    Neatened attack code

--- new experiments/attack.py
+++ new experiments/attack.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import os
 
 import numpy as np
 import jax
@@ -12,6 +13,7 @@ from tqdm import tqdm, trange
 import matplotlib.pyplot as plt
 import sklearn.metrics as skm
 import skimage.metrics as skim
+import pandas as pd
 
 import models
 import load_datasets
@@ -47,7 +49,7 @@ def representation_loss(state, true_reps, lamb_tv=1e-4):
     return _apply
 
 
-def plot_image_array(images, labels):
+def plot_image_array(images, labels, filename):
     # Order the data according to label values
     idx = np.argsort(labels)
     images = images[idx]
@@ -69,6 +71,7 @@ def plot_image_array(images, labels):
         plt.title(f"Label: {labels[0]}")
         plt.imshow(images[0], cmap="binary")
     plt.tight_layout()
+    plt.savefig(filename, dpi=320)
     plt.show()
 
 
@@ -80,15 +83,45 @@ def measure_leakage(true_X, Z, true_Y, labels):
             metrics['psnr'].append(skim.peak_signal_noise_ratio(tx, z, data_range=1.0))
     return {"ssim": max(metrics['ssim']), "psnr": max(metrics['psnr'])}
 
+
+def perform_attack(state, dataset, attack, train_args, seed=42):
+    batch_size = int(train_args['batch_size'])
+    idx = np.random.default_rng(seed).choice(len(dataset['train']['Y']), batch_size)
+    update_step = common.pgd_update_step if train_args["pgd"] else common.update_step
+    loss, new_state = update_step(state, dataset['train']['X'][idx], dataset['train']['Y'][idx])
+
+    true_grads = jax.tree_util.tree_map(lambda a, b: a - b, state.params, new_state.params)
+    labels = jnp.argsort(jnp.min(true_grads['params']['classifier']['kernel'], axis=0))[:batch_size]
+
+    if attack == "representation":
+        true_reps = true_grads['params']['classifier']['kernel'].T[labels]
+        solver = jaxopt.OptaxSolver(representation_loss(state, true_reps), optax.adam(0.01), pre_update=lambda X, s: ((X - X.min()) / (X.max() - X.min()), s))
+    else:
+        solver = jaxopt.LBFGS(idlg_loss(state, update_step, true_grads, labels))
+
+    Z = jax.random.normal(jax.random.PRNGKey(seed), shape=(batch_size,) + dataset.input_shape) * 0.2 + 0.5
+    attack_state = solver.init_state(Z)
+    trainer = jax.jit(solver.update)
+    for _ in (pbar := trange(1000)):
+        Z, attack_state = trainer(Z, attack_state)
+        pbar.set_postfix_str(f"LOSS: {attack_state.value:.5f}")
+    Z = (Z - Z.min()) / (Z.max() - Z.min())
+    if isinstance(Z, tuple):
+        Z, labels = Z
+        labels = np.argmax(labels, axis=-1)
+    Z, labels = np.array(Z), np.array(labels)
+    return Z, labels, idx
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train neural network models for inversion attacks.")
     parser.add_argument('-f', '--folder', type=str, required=True, help="Folder containing the checkpoints of the model")
-    parser.add_argument('-s', '--seed', type=int, default=42, help="Seed for random number generation operations.")
+    parser.add_argument('-r', '--runs', type=int, default=1, help="Number of runs of the attack to perform.")
+    parser.add_argument('-p', '--plot', action="store_true", help="Whether to plot the final results.")
     parser.add_argument('-a', '--attack', type=str, default="representation", help="The type of gradient inversion attack to perform.")
     args = parser.parse_args()
 
     train_args = {a.split('=')[0]: a.split('=')[1] for a in args.folder[args.folder.rfind('/') + 1:].split('-')}
-    batch_size = int(train_args['batch_size'])
     dataset = getattr(load_datasets, train_args['dataset'])()
     model = getattr(models, train_args["model"])(dataset.nclasses)
     try:
@@ -107,34 +140,25 @@ if __name__ == "__main__":
     )
     state = ckpt_mgr.restore(int(train_args["epochs"]) - 1, state, restore_kwargs={'restore_args': orbax_utils.restore_args_from_target(state, mesh=None)})
 
-    idx = np.random.default_rng(args.seed).choice(len(dataset['train']['Y']), batch_size)
-    update_step = common.pgd_update_step if train_args["pgd"] else common.update_step
-    loss, new_state = update_step(state, dataset['train']['X'][idx], dataset['train']['Y'][idx])
-
-    true_grads = jax.tree_util.tree_map(lambda a, b: a - b, state.params, new_state.params)
-    labels = jnp.argsort(jnp.min(true_grads['params']['classifier']['kernel'], axis=0))[:batch_size]
-
-    if args.attack == "representation":
-        true_reps = true_grads['params']['classifier']['kernel'].T[labels]
-        solver = jaxopt.OptaxSolver(representation_loss(state, true_reps), optax.adam(0.01), pre_update=lambda X, s: ((X - X.min()) / (X.max() - X.min()), s))
-    else:
-        solver = jaxopt.LBFGS(idlg_loss(state, update_step, true_grads, labels))
-
-    Z = jax.random.normal(jax.random.PRNGKey(args.seed), shape=(batch_size,) + dataset.input_shape) * 0.2 + 0.5
-    attack_state = solver.init_state(Z)
-    trainer = jax.jit(solver.update)
-    for _ in (pbar := trange(1000)):
-        Z, attack_state = trainer(Z, attack_state)
-        pbar.set_postfix_str(f"LOSS: {attack_state.value:.5f}")
-    Z = (Z - Z.min()) / (Z.max() - Z.min())
-
-    # Plot the results
-    if isinstance(Z, tuple):
-        Z, labels = Z
-        labels = jnp.argmax(labels, axis=-1)
-    Z, labels = np.array(Z), np.array(labels)
-    print("Ground truth")
-    plot_image_array(dataset['train']['X'][idx], dataset['train']['Y'][idx])
-    print("Attack images")
-    plot_image_array(Z, labels)
-    print(measure_leakage(dataset['train']['X'][idx], Z, dataset['train']['Y'][idx], labels))
+    all_results = {k: [v for _ in range(args.runs)] for k, v in train_args.items() if k in ["dataset", "model", "optimiser", "pgd"]}
+    all_results.update({"seed": [], "psnr": [], "ssim": []})
+    for i in range(0, args.runs):
+        seed = round(np.e**i + np.e**(i - 1) * np.cos(i * np.pi / 2)) % 2**31
+        print(f"Performing the attack with {seed=}")
+        Z, labels, idx = perform_attack(state, dataset, args.attack, train_args, seed)
+        results = measure_leakage(dataset['train']['X'][idx], Z, dataset['train']['Y'][idx], labels)
+        for k, v in results.items():
+            all_results[k].append(v)
+        all_results["seed"].append(seed)
+        print(f"Attack performance: {results}")
+    if args.plot:
+        os.makedirs("attack_images", exist_ok=True)
+        print("Ground truth")
+        plot_image_array(dataset['train']['X'][idx], dataset['train']['Y'][idx], "attack_images/ground_truth.png")
+        print("Attack images")
+        plot_image_array(
+            Z,
+            labels,
+            f"attack_images/{args.attack}_{train_args['model']}_{train_args['dataset']}_{train_args['optimiser']}{'_pgd' if train_args['pgd'] == 'True' else ''}.png"
+        )
+    pd.DataFrame.from_dict(all_results).to_csv("results.csv", mode='a', header=not os.path.exists("results.csv"), index=False)
