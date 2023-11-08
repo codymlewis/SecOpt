087e82f92c3880c7fec38711fab8505c7d29023e
Cody Lewis <hello@codymlewis.com>
2023-11-08 14:31:27 +1100

    Added some further arguments to the ablation

--- inversion/ablation.py
+++ inversion/ablation.py
@@ -2,7 +2,6 @@ import argparse
 import math
 import os
 from functools import partial
-import shutil
 
 import numpy as np
 import jax
@@ -75,39 +74,50 @@ if __name__ == "__main__":
     parser.add_argument('-ps', '--pool-size', type=str, default="small",
                         help="Specify a window size for the pooling layers. [small or large]")
     parser.add_argument('-n', '--normalisation', type=str, default="none", help="Type of normalisation layers to apply.")
+    parser.add_argument('-e', '--train_epochs', type=int, default=10, help="The number of epochs to perform training for.")
+    parser.add_argument('-d', '--dataset', type=str, default="cifar10", help="Dataset to train on.")
+    parser.add_argument('--attack', type=str, default="representation", help="The attack to perform.")
     args = parser.parse_args()
 
     seed = 56
     batch_size = 8
-    train_epochs = 10
     attack_runs = 100
     net_config = vars(args)
     print(f"Running ablation with {vars(args)}")
 
     rng = np.random.default_rng(seed)
-    dataset = load_datasets.cifar10()
-    model = CNN(dataset.nclasses, **net_config)
+    dataset = getattr(load_datasets, args.dataset)()
+    model = CNN(
+        dataset.nclasses,
+        activation=args.activation,
+        pooling=args.pooling,
+        pool_size=args.pool_size,
+        normalisation=args.normalisation
+    )
     state = train_state.TrainState.create(
         apply_fn=model.apply,
         params=model.init(jax.random.PRNGKey(seed), dataset['train']['X'][:1]),
         tx=optax.sgd(0.001),
     )
-    checkpoint_folder = "checkpoints/{}".format('-'.join([f'{k}={v}' for k, v in net_config.items()]))
-    # shutil.rmtree(checkpoint_folder, ignore_errors=True)
+    checkpoint_folder = "checkpoints/{}".format('-'.join([f'{k}={v}' for k, v in net_config.items() if k not in ['attack']]))
     ckpt_mgr = ocp.CheckpointManager(
         checkpoint_folder,
         ocp.Checkpointer(ocp.PyTreeCheckpointHandler()),
         options=ocp.CheckpointManagerOptions(create=True, keep_period=1),
     )
 
-    if os.path.exists(checkpoint_folder):
+    if os.path.exists(f"{checkpoint_folder}/{args.train_epochs - 1}"):
         state = ckpt_mgr.restore(
-            train_epochs - 1, state, restore_kwargs={'restore_args': orbax_utils.restore_args_from_target(state, mesh=None)}
+            args.train_epochs - 1,
+            state,
+            restore_kwargs={'restore_args': orbax_utils.restore_args_from_target(state, mesh=None)}
         )
         print(f"Checkpoint loaded from {checkpoint_folder}")
     else:
-        for e in (pbar := trange(train_epochs)):
-            idxs = np.array_split(rng.permutation(len(dataset['train']['Y'])), math.ceil(len(dataset['train']['Y']) / batch_size))
+        for e in (pbar := trange(args.train_epochs)):
+            idxs = np.array_split(
+                rng.permutation(len(dataset['train']['Y'])), math.ceil(len(dataset['train']['Y']) / batch_size)
+            )
             loss_sum = 0.0
             for idx in idxs:
                 loss, state = common.update_step(state, dataset['train']['X'][idx], dataset['train']['Y'][idx])
@@ -121,11 +131,14 @@ if __name__ == "__main__":
 
     all_results = {k: [v for _ in range(attack_runs)] for k, v in net_config.items()}
     all_results["accuracy"] = [final_accuracy for _ in range(attack_runs)]
+    all_results["attack"] = [args.attack for _ in range(attack_runs)]
     all_results.update({"seed": [], "psnr": [], "ssim": []})
     for i in range(0, attack_runs):
         attack_seed = round(i**2 + i * np.cos(i * np.pi / 4)) % 2**31
         print(f"Performing the attack with {attack_seed=}, {i=}")
-        Z, labels, idx = attack.perform_attack(state, dataset, "representation", {"batch_size": batch_size, "pgd": False}, attack_seed)
+        Z, labels, idx = attack.perform_attack(
+            state, dataset, args.attack, {"batch_size": batch_size, "pgd": False}, attack_seed
+        )
         results = attack.measure_leakage(dataset['train']['X'][idx], Z, dataset['train']['Y'][idx], labels)
         tuned_Z = attack.tune_brightness(Z.copy(), dataset['train']['X'][idx])
         tuned_results = attack.measure_leakage(dataset['train']['X'][idx], tuned_Z, dataset['train']['Y'][idx], labels)
