2ca75383dbc25cb821bd3d1950c3430fe438b20b
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2023-11-15 15:17:25 +1100

    Updated jax and ablation script

--- inversion/ablation.sh
+++ inversion/ablation.sh
@@ -11,9 +11,8 @@ for activation in "relu" "elu" "sigmoid" "leaky_relu" "tanh"; do
 		
 		for extra_flag in "${extra_flags[@]}"; do
 			for normalisation in "none" "LayerNorm"; do
-				# for attack in "representation" "idlg" "reg_idlg"; do
-				for attack in "representation"; do
-					python ablation.py -a $activation -p $pooling $extra_flag -n $normalisation --attack $attack --batch-size 1
+				for attack in "representation" "idlg" "reg_idlg"; do
+					python ablation.py -a $activation -p $pooling $extra_flag -n $normalisation --attack $attack --batch-size 1 --zinit repeated_pattern
 				done
 			done
 		done
