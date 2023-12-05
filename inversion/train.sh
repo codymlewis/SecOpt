99f4edba9071e4b56e805af61d65d4aa5d1b7f43
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2023-12-05 16:34:45 +1100

    Fixed training script and added attack information

--- inversion/train.sh
+++ inversion/train.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 
-# for dataset in fmnist cifar10 cifar100 svhn tinyimagenet; do
-for dataset in tinyimagenet; do
-    for optimiser in "secadam"; do
-        if [[ $dataset == "fmnist" ]]; then
-            models=("CNN" "LeNet")
-        else
-            models=("ConvNeXt")
-        fi
-        if [[ $dataset == "cifar100" ]] || [[ $dataset == "tinyimagenet" ]]; then
-            batch_size=32
-        else
-            batch_size=8
-        fi
+for dataset in fmnist cifar10 cifar100 svhn tinyimagenet; do
+    if [[ $dataset == "fmnist" ]]; then
+        models=("CNN" "LeNet")
+    else
+        models=("CNN" "ConvNeXt")
+    fi
+    if [[ $dataset == "cifar100" ]] || [[ $dataset == "tinyimagenet" ]]; then
+        batch_size=32
+    else
+        batch_size=8
+    fi
 
-        for model in ${models[@]}; do
-            # for other_flags in "" "--pgd" "--perturb" "--pgd --perturb"; do
-            for other_flags in ""; do
-                python train.py --epochs 50 --dataset $dataset --optimiser $optimiser --model $model --batch-size $batch_size $other_flags
-            done
+    for model in ${models[@]}; do
+        for other_flags in ""; do
+            python train.py --epochs 50 --dataset $dataset --optimiser secadam --model $model --batch-size $batch_size $other_flags
         done
     done
 done
