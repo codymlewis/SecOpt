8d83954300866db0dcfb69487df3da4772c37daa
Cody Lewis <hello@codymlewis.com>
2023-12-13 09:06:47 +1100

    Added some more result handling

--- inversion/statistics.py
+++ inversion/statistics.py
@@ -1,11 +1,8 @@
-from scipy import stats
 import pandas as pd
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("ablation_results.csv")
-    act_vals = []
-    for act in df['activation'].unique():
-        act_vals.append(df[df['activation'] == act]['ssim'].to_numpy())
-        print(f"Activation {act}, mean SSIM {act_vals[-1].mean()}")
-    print(stats.f_oneway(*act_vals))
+    df = pd.read_csv("results.csv")
+    df = df.groupby(['dataset', 'model', 'pgd', 'attack', 'optimiser', 'batch_size']).mean()
+    df = df.reset_index()
+    print(df.style.to_latex(position_float='centering'))
