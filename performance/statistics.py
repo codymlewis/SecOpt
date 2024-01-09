ed2a72553d484e36f0e7f9a484715d1292319553
Cody Lewis <hello@codymlewis.com>
2024-01-10 09:29:22 +1100

    Improved performance statistics

--- performance/statistics.py
+++ performance/statistics.py
@@ -3,7 +3,19 @@ import pandas as pd
 
 if __name__ == "__main__":
     df = pd.read_csv("results.csv")
-    df = df.drop(columns=["seed", "clients", "rounds", "epochs", "steps", "batch_size"])
-    df = df.groupby(['dataset', 'model', 'pgd', 'server_optimiser', 'client_optimiser']).mean()
+    df = df.drop(columns=[
+        "seed",
+        "clients",
+        "rounds",
+        "epochs",
+        "steps",
+        "batch_size",
+        "participation_rate",
+        "client_learning_rate",
+        "server_learning_rate",
+    ])
+    df = df.groupby([
+        'dataset', 'model', 'pgd', 'server_optimiser', 'client_optimiser', 'clip_threshold', 'noise_scale'
+    ]).mean()
     df = df.reset_index()
-    print(df.style.to_latex(position_float='centering'))
+    print(df.style.hide().to_latex(position_float='centering'))
