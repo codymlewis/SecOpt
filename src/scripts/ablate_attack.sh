3f0c0cf333bb4d463214a43510a71973f3305f44
Cody Lewis <hello@codymlewis.com>
2024-04-24 10:20:46 +1000

    Added attack ablation

---
+++ src/scripts/ablate_attack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for checkpoint in checkpoints/*; do
+  for l1_reg in 0.0 0.1 0.01 0.001 0.0001 0.00001 0.000001; do
+    for l2_reg in 0.0 0.1 0.01 0.001 0.0001 0.00001 0.000001; do
+    	python attack.py -f $checkpoint -r 5 -b 8 --optimiser sgd --zinit data --l1-reg $l1_reg --l2-reg $l2_reg
+  	done
+	done
+done
