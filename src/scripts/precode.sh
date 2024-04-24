8ad56373afa25aca95e2b4087727f58efec85bd7
Cody Lewis <hello@codymlewis.com>
2024-04-25 09:11:15 +1000

    Gave precode the ablated parameters

--- src/scripts/precode.sh
+++ src/scripts/precode.sh
@@ -61,7 +61,24 @@ done
 echo "Inverting models..."
 
 for checkpoint in precode_checkpoints/*; do
-  python precode.py -f $checkpoint --runs 30 -b 8 --attack --l1-reg 0.0 --l2-reg 0.001
+  if echo "$checkpoint" | grep -q 'cifar100'; then
+		l1_reg="0.0"
+		l2_reg="0.0001"
+	elif echo "$checkpoint" | grep -q 'cifar10'; then
+		l1_reg="0.00001"
+		l2_reg="0.0001"
+	elif echo "$checkpoint" | grep -q 'fmnist'; then
+		l1_reg="0.000001"
+		l2_reg="0.000001"
+	elif echo "$checkpoint" | grep -q 'svhn'; then
+		l1_reg="0.00001"
+		l2_reg="0.00001"
+	elif echo "$checkpoint" | grep -q 'tinyimagenet'; then
+		l1_reg="0.000001"
+		l2_reg="0.0001"
+	fi
+
+  python precode.py -f $checkpoint --runs 30 -b 8 --attack --l1-reg $l1_reg --l2-reg $l2_reg
 done
 
 echo "Done."
